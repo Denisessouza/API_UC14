b27adad16320dd4f6ae6d8f1ec13cef804c78800
Denise Souza <95714393+Denisessouza@users.noreply.github.com>
2022-09-30 23:15:24 -0300

    Encontro Remoto 1 - Estrutura da API

---
+++ ER1/Chapter/Chapter/Contexts/ChapterContext.cs
@@ -0,0 +1,35 @@
+﻿using Chapter.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Chapter.Contexts
+{
+    public class ChapterContext: DbContext
+    {
+        public ChapterContext()
+        {
+
+        }
+
+        public ChapterContext(DbContextOptions<ChapterContext> options): base(options)
+        {
+
+        }
+        //Método a seguir para configurar o BANCO DE DADOS
+
+        protected override void OnConfiguring (DbContextOptionsBuilder optionsBuilder)
+        {
+            if(!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source = DENISE-SOUZA\\SQLEXPRESS; initial catalog = Chapter; Integrated Security = true");
+                    //Se estivesse usando Nuvem aqui eu colocaria o Conection strip algo assim
+                    //Cada provedor term sua sintaxe, trocou de banco tem que mudar essa linha
+            }
+        }
+        //DbSet representa as entidades que serão utilizadas nas operações do Crud
+        public DbSet <Livro> Livros { get; set; }
+    }
+
+
+}
+
+
